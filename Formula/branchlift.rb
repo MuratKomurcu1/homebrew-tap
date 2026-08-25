d139f7cbea9bbc5923c1d8bb991d09d1ff11d92a
Murat Komurcu <muratkomurrcu@gmail.com>
2026-08-25 04:45:39 +0300

    branchlift 1.2.0

--- Formula/branchlift.rb
+++ Formula/branchlift.rb
@@ -1,8 +1,8 @@
 class Branchlift < Formula
   desc "Stateful backend environments for parallel coding agents"
-  homepage "https://github.com/muratkomurcu/BranchLift"
-  url "https://github.com/muratkomurcu/BranchLift/releases/download/v1.1.0/branchlift-1.1.0.tgz"
-  sha256 "b1b3f153510b112106f0d1f07eef983a91e1249e461dd33740b2e36fbd5c3128"
+  homepage "https://github.com/MuratKomurcu1/BranchLift"
+  url "https://github.com/MuratKomurcu1/BranchLift/releases/download/v1.2.0/branchlift-1.2.0.tgz"
+  sha256 "30de1b976791f09bb0e8ff46a9462d583dba90e8033f649bd2a949e1282a3b34"
   license "Apache-2.0"
 
   depends_on "node"
